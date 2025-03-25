3a9744d9012ee8d94755f63bcfd07021c1563086
KelvinF87 <docentekj@gmail.com>
2025-03-25 22:09:20 +0000

    ajuste de llamada a la api 3 opciones

--- js/config/api.js
+++ js/config/api.js
@@ -1,2 +1,21 @@
-// export const API_ENDPOINT = "http://localhost:5005/api"; // Replace with your actual API endpoint
-export const API_ENDPOINT = "https://serviairemote.onrender.com/api"; // Replace with your actual API endpoint
+const API_ENDPOINT_PRIMARY = process.env.API_ENDPOINT_PRIMARY || "http://localhost:5005/api";
+const API_ENDPOINT_SECONDARY = "https://serviairemote.onrender.com/api";
+const API_ENDPOINT_TERTIARY = "http://docentek.ddnst.net:5005/api";
+
+let API_ENDPOINT = API_ENDPOINT_PRIMARY; // Inicialmente usa la URL primaria
+let currentEndpointIndex = 0; // Índice del endpoint actual en uso
+
+const API_ENDPOINTS = [
+    API_ENDPOINT_PRIMARY,
+    API_ENDPOINT_SECONDARY,
+    API_ENDPOINT_TERTIARY,
+];
+
+// Función para cambiar la URL de forma global (por si detectas un fallo en tiempo de ejecución)
+export const setNextBackupAPI = () => {
+    currentEndpointIndex = (currentEndpointIndex + 1) % API_ENDPOINTS.length; // Avanza al siguiente endpoint
+    API_ENDPOINT = API_ENDPOINTS[currentEndpointIndex];
+    console.warn(`Switching to API endpoint: ${API_ENDPOINT}`);
+};
+
+export { API_ENDPOINT };
